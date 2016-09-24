99fe4d9a4fd874ccf356ae960e27e31ba85738c3
Joseph McGovern <joseph.mcgovern@webfilings.com>
2016-09-24 18:07:29 -0500

    api key

--- src/main/java/ocr_reader/OCRReader.java
+++ src/main/java/ocr_reader/OCRReader.java
@@ -16,3 +16,4 @@ public class OCRReader {
 		return result;
 	}
 }
+
--- src/main/java/ocr_reader/OCRReaderRequest.java
+++ src/main/java/ocr_reader/OCRReaderRequest.java
@@ -2,6 +2,8 @@ package ocr_reader;
 
 public class OCRReaderRequest {
 	
+	private static final String API_KEY = "fa03bcc77488957";
+	
 	public String imageUrl;
 	
 	public OCRReaderRequest(String imageUrl) {
