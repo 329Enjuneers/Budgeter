df5d7d2fa25ce3f53e82bb94f2310c3e7b203878
Hope Scheffert <hope_scheffert@domain.com>
2016-09-29 20:19:43 -0500

    little in expense page

--- src/main/java/pages/ExpensePage.java
+++ src/main/java/pages/ExpensePage.java
@@ -3,15 +3,22 @@ package pages;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import budgeter.Expense;
 import pages.html_builder.Form;
 
 
 public class ExpensePage extends Page 
 {
+	private Expense expense;
+	
 	public ExpensePage(String baseUrl) 
 	{
 		super(baseUrl);
 		htmlBuilder.includeAppHeader = true;
+		//TODO where does id come from?
+		expense = new Expense("walmart","food" ,50.0);
+		
+		
 	}
 
 	public String make() 
@@ -21,7 +28,8 @@ public class ExpensePage extends Page
 	    	addLogout();
 	    	return htmlBuilder.build();
 	    }
-	    addNewGroupForm();
+	    //addNewGroupForm();
+	    addExpenseInfo();
 	    addHorizontalRule();
 	    return htmlBuilder.build();
 	}
@@ -33,20 +41,43 @@ public class ExpensePage extends Page
 		} catch (Exception e) {}
 	}
 
-	private void addNewGroupForm() 
+//	private void addNewGroupForm() 
+//	{
+//		Form newGroupForm = new Form();
+//	    newGroupForm.addProperty("action", "/");
+//	    newGroupForm.addProperty("method", "POST");
+//	    newGroupForm.addProperty("style", "margin-bottom:2em");
+//	    newGroupForm.addElement("<div style='margin-bottom: 1em'><label><b>New Group</b></label></div>");
+//	    newGroupForm.addElement("<input name='group-name' placeholder='Group Name' required>");
+//	    newGroupForm.addElement("<button type='submit'>Add Group</button>");
+//	    htmlBuilder.addToBody(newGroupForm.toString());
+//	}
+	/**
+	 * store (editable)
+	 * amount (editable)
+	 * date (editable)
+	 */
+	private void addExpenseInfo()
 	{
-		Form newGroupForm = new Form();
-	    newGroupForm.addProperty("action", "/");
-	    newGroupForm.addProperty("method", "POST");
-	    newGroupForm.addProperty("style", "margin-bottom:2em");
-	    newGroupForm.addElement("<div style='margin-bottom: 1em'><label><b>New Group</b></label></div>");
-	    newGroupForm.addElement("<input name='group-name' placeholder='Group Name' required>");
-	    newGroupForm.addElement("<button type='submit'>Add Group</button>");
-	    htmlBuilder.addToBody(newGroupForm.toString());
-	}
-	
-	private void showExpenseInfo()
-	{
+		String expenseSummary = "<div id='expense-summary'>";
+		expenseSummary += "<table>";
+		expenseSummary += "<thead><tr>";
+		expenseSummary += "<th colspan=\"2\">Expense Information</th>";
+		expenseSummary += "<th></th>";
+		expenseSummary += "<tr>";
+		expenseSummary += "</tr></thead>";
+		expenseSummary += "<tbody><tr>";
+		expenseSummary += "<td><strong>Store</strong></td><td>&nbsp;"+expense.store+"</td>";
+		expenseSummary += "</tr><tr>";
+		expenseSummary += "<td><strong>Amount</strong></td><td>&nbsp;"+expense.amount+"</td>";
+		expenseSummary += "</tr><tr>";
+		expenseSummary += "<td><strong>Date</strong></td><td>&nbsp;"+expense.date+"</td>";
+		expenseSummary += "</tr>";
+		expenseSummary += "</tbody>";
+		expenseSummary += "</table>";
+		expenseSummary += "</div><br/>";
+		expenseSummary += "<hr/><br/>";
+		htmlBuilder.addToBody(expenseSummary);
 		
 	}
 
